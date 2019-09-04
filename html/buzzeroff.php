dfc3c159afc3d83be315ac8bfe874ee4099fe5ca
kongkubpan <54573395+kongkubpan@users.noreply.github.com>
2019-09-04 11:11:25 +0700

    Add files via upload

---
+++ html/buzzeroff.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * buzzer.php
+ * 
+ * Copyright 2019  <pi@piB>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+exec("sudo python /home/pi/Desktop/status/buzzeroff.py");
+echo "Buzzer change";
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>untitled</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 1.29" />
+</head>
+
+<body>
+	
+</body>
+
+</html>
